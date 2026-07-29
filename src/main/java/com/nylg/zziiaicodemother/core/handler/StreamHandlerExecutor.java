f0f3ed2f26404b6201b1b41cc5a7fb063ecc8b48
zzii <2059118566@qq.com>
2026-07-29 18:19:32 +0800

    第六期-工程项目生成-工程项目构建，浏览和部署。

--- src/main/java/com/nylg/zziiaicodemother/core/handler/StreamHandlerExecutor.java
+++ src/main/java/com/nylg/zziiaicodemother/core/handler/StreamHandlerExecutor.java
@@ -38,9 +38,9 @@ public class StreamHandlerExecutor {
                                   ChatHistoryService chatHistoryService,
                                   long appId, User loginUser, CodeGenTypeEnum codeGenType) {
         return switch (codeGenType) {
-            case VUE_PROJECT -> // 使用注入的组件实例
+            case VUE_PROJECT ->
                     jsonMessageStreamHandler.handle(originFlux, chatHistoryService, appId, loginUser);
-            case HTML, MULTI_FILE -> // 简单文本处理器不需要依赖注入
+            case HTML, MULTI_FILE ->
                     simpleTextStreamHandler.handle(originFlux, chatHistoryService, appId, loginUser);
         };
     }
